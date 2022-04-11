9cfe824cbcf517e9f4245ff4a436a99ee21a17af
Nicolas SWIGON <swigon.nicolas@gmail.com>
2022-04-11 13:01:08 +0200

    correction Nicolas

--- M11001-1.php
+++ M11001-1.php
@@ -3,23 +3,32 @@
 /* EXERCICE 1 */
     // Déclarer une variable nommée $text qui contient le texte « Bonjour »
 
+    $text="Bonjour";
+
     // Déclarer une autre variable, qui contiendra « à tout le monde les gens ! »
+    $all = "à tout le monde les gens ! ";
 
     // Concaténer les deux variables
+    echo "$text $all";
 
 
 
 /* EXERCICE 2 */
     // Puis une variable $number1 égale à 124 et une variable $number2 qui vaut 378.24
+    $number1 = 124;
+    $number2 = 378.24;
 
     // Effectuer l’addition de $number1 et $number2
-
+    echo $number1 + $number2;
+   
 
 
 /* EXERCISE 3 */
     // Corriger la déclaration de la variable suivante avec la méthode de votre choix
     // $exercice3 = " On l'appelle "Trinita" ";
-
+    echo " On l'appelle \"Trinita\" ";
+   
 
 /* EXERCISE 4 */
     // Créer une variable simple booléenne ayant pour valeur 0
+    $bool = false;
--- M11001-2.php
+++ M11001-2.php
@@ -1,6 +1,22 @@
 <?php
 
 // Créer une constante avec la fonction adaptée, et lui donner le nom et la valeur 18
+define("TEL", 18);
+
+// Utiliser cette constante dans une nouvelle variable de type string, ayant pour contenu "Le numéro des pompiers est le " puis concaténer votre constante
+$variable =  "Le numéro des pompiers est le " . TEL;
+
+echo $variable;
 
 
-// Utiliser cette constante dans une nouvelle variable de type string, ayant pour contenu "Le numéro des pompiers et le " puis concaténer votre constante
+// Concaténer les chaines de caractères suivantes :
+//  - La référence "19805" est 
+//  - l'appareil le plus vendu dans
+//  - l'agglomération de "Pont-l'évêque"
+
+echo "<br>";
+$text1 = "La référence \"19805\" est";
+$text2 = "l'appareil le plus vendu dans";
+$text3 = "l'agglomération de \"Pont-l'évêque\" ";
+
+echo "$text1 $text2 $text3";
